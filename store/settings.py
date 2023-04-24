4857a56d64477c3ef514317d621d49560a078cac
Andrey Sipatov <Henrrry52@gmail.com>
2023-04-24 11:26:26 +0400

    Add models. Create fixtures. Filled templates form models

--- store/settings.py
+++ store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
